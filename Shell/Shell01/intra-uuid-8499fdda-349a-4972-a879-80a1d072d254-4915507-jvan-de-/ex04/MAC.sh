b81c24b0e3f7ae5c488a408a1ed32b746ae9862f
Brxh <98915295+RealBrxh@users.noreply.github.com>
2023-07-25 14:27:22 +0200

    Reuploading

--- Shell/Shell01/intra-uuid-8499fdda-349a-4972-a879-80a1d072d254-4915507-jvan-de-/ex04/MAC.sh
+++
@@ -1,1 +0,0 @@
-ifconfig | grep -Eo '([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}'
--- Shell/Shell01/intra-uuid-8499fdda-349a-4972-a879-80a1d072d254-4915507-jvan-de-/ex07/r_dwssap.sh
+++
@@ -1,2 +0,0 @@
-cat /etc/passwd | grep -v '^#' | awk 'NR % 2 == 0' | rev | sort -r | awk -v l1="$1" -v l2="$5" -F ':' 'NR >= l1 && NR <= l2 { printf "%s, ", $1 } END { if (NR >= l1 && NR <= l2) printf "\b\b.\n"; else print "" }'
-
